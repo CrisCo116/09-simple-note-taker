f5691e8203a12ee6cbbfccda695a2b20654cfa73
Chris <138630942+CrisCo116@users.noreply.github.com>
2023-11-18 06:53:38 -0800

    lets see if this one works?

--- server.js
+++ server.js
@@ -1,105 +1,57 @@
-// Import the Express.js framework
 const express = require('express');
-// Import the 'path' module for file path handling
-const path = require('path');
-// Import the 'fs' module for file system operations
 const fs = require('fs');
-// Import the 'uuid' module and rename the v4 function as 'uuidv4'
-const { v4: uuidv4 } = require('uuid');
-// Create an instance of the Express application
+const { v4: uuidv4 } = require('uuid'); // Use the uuid package for generating unique IDs
+const path = require('path')
 const app = express();
-// Port number for the server
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-app.use(express.static('public'));
-app.use(express.json());
+app.use(express.json()); // Parse JSON request bodies
+app.use(express.static("public"))
 
-// Serve the HTML pages index & notes
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+app.get('/notes',(req,res)=>{
+    res.sendFile(path.join(__dirname,'./public/notes.html'))
+})
 
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'notes.html'));
-});
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'./public/index.html'))
+})
+// app.get('/assets/css/styles.css',(req,res)=>{
+//     res.sendFile(path.join(__dirname,'./public/assets/css/styles.css'))
+// })
 
-// API Routes
-// Listens for HTTP GET requests at the path
-app.get('/api/notes', (req, res) => {
-  fs.readFile('db/db.json', 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      res.status(500).json({ error: 'Internal Server Error' });
-    } else {
-      const notes = JSON.parse(data);
-      res.json(notes);
-    }
-  });
-});
+app.get('/api/notes',(req,res)=>{
+    fs.readFile("./db/db.json","utf-8",(err,content)=>{
+        if(err){
+            console.log(err)
+        }else{
+            var data = JSON.parse(content)
+            res.json(data)
+        }
+    })
+})
 
-// Listens for HTTP POST requests at the path
+// Endpoint for creating a new note
 app.post('/api/notes', (req, res) => {
-  fs.readFile('db/db.json', 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      res.status(500).json({ error: 'Internal Server Error' });
-      return;
-    }
+  try {
+    const newNote = req.body; // Assuming the request body contains the new note
+    newNote.id = uuidv4(); // Generate a unique ID for the note
 
-    const notes = JSON.parse(data);
+    // Read existing notes from the db.json file
+    const existingNotes = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
 
-    const newNote = {
-      id: uuidv4(),
-      title: req.body.title,
-      text: req.body.text,
-    };
+    // Add the new note to the existing notes
+    existingNotes.push(newNote);
 
-    notes.push(newNote);
+    // Write the updated notes back to the db.json file
+    fs.writeFileSync('./db/db.json', JSON.stringify(existingNotes, null, 2), 'utf-8');
 
-    fs.writeFile('db/db.json', JSON.stringify(notes), (err) => {
-      if (err) {
-        console.error(err);
-        res.status(500).json({ error: 'Internal Server Error' });
-        return;
-      }
-
-      res.json(newNote);
-    });
-  });
+    res.status(201).json(newNote); // Respond with the new note, including its unique ID
+  } catch (error) {
+    console.error('Error creating a new note:', error);
+    res.status(500).json({ error: 'An error occurred while creating a new note.' });
+  }
 });
 
-//  Listens for HTTP DELETE requests at the path
-app.delete('/api/notes/:id', (req, res) => {
-  const noteId = req.params.id;
-
-  fs.readFile('db/db.json', 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      res.status(500).json({ error: 'Internal Server Error' });
-    } else {
-      let notes = JSON.parse(data);
-
-      const index = notes.findIndex((note) => note.id === noteId);
-
-      if (index !== -1) {
-        notes.splice(index, 1);
-
-        fs.writeFile('db/db.json', JSON.stringify(notes), (err) => {
-          if (err) {
-            console.error(err);
-            res.status(500).json({ error: 'Internal Server Error' });
-          } else {
-            res.json({ message: 'Note deleted successfully' });
-          }
-        });
-      } else {
-        res.status(404).json({ error: 'Note not found' });
-      }
-    }
-  });
-});
- 
-// Start Server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
